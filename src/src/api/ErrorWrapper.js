cdb6f97ee2a418f6005f2817c9115d51fb2477ee
DESKTOP-HKGEPO2\duong <dqvinh20@clc.fitus.edu.vn>
2023-09-24 23:21:52 +0700

    Add ErrorWrapper to wrap try catch code MU-9

---
+++ src/src/api/ErrorWrapper.js
@@ -0,0 +1,21 @@
+const wrapperFunction = (fn) => {
+  return async (...args) => {
+    try {
+      const result = fn.apply(this, args);
+      return result;
+    } catch (ex) {
+      throw new Error(ex.message);
+    }
+  };
+};
+
+export default (object) => {
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    if (object[key] && typeof object[key] === "function") {
+      // Wrap function with try catch
+      result[key] = wrapperFunction(object[key]);
+    }
+  });
+  return result;
+};
