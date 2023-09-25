aa089f187f1dc8610c98dd7225ea34551c2c703f
DESKTOP-HKGEPO2\duong <dqvinh20@clc.fitus.edu.vn>
2023-09-25 12:18:41 +0700

    Add JSDoc MU-9

--- src/src/api/ErrorWrapper.js
+++ src/src/api/ErrorWrapper.js
@@ -1,14 +1,26 @@
+/**
+ * This function wrap async/await function with try catch block
+ *
+ * @param {function(...args) : Promise<any> | any} fn
+ * @returns {function(...args) : Promise<any>}
+ */
 const wrapperFunction = (fn) => {
   return async function _dummyFunc(...args) {
     try {
       const result = await fn.apply(this, args);
       return result;
-    } catch (ex) {
-      throw new Error(ex.message);
+    } catch (error) {
+      throw new Error(error.message);
     }
   };
 };
 
+/**
+ * Wrap all function in object with try catch block
+ * Don't need to wrap function in object manually
+ * @param {object} object
+ * @returns {object}
+ */
 export default (object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
