b3cdc3e8f4b717ddc792be6dda3ea1d38069d5e7
DESKTOP-HKGEPO2\duong <dqvinh20@clc.fitus.edu.vn>
2023-09-25 11:22:17 +0700

    Refactor and wrap GroupCategoryApi with ErrorWrapper MU-9

--- src/src/api/GroupCategoryApi.js
+++ src/src/api/GroupCategoryApi.js
@@ -1,102 +1,55 @@
 import { makeRequestSearch } from "utils";
 import AxiosClient from "./AxiosClient";
+import ErrorWrapper from "./ErrorWrapper";
 
-export default class GroupCategoryApi {
-  static async all() {
-    let categories = [];
-    try {
-      const response = await AxiosClient.get(`api/group-categories`);
-      categories = response.data;
-    } catch (error) {
-      throw new Error(error.message);
-    }
-    return categories;
+const GroupCategoryApi = {
+  all() {
+    return AxiosClient.get(`api/group-categories`).then((response) => response.data);
+  },
+
+  findById(categoryID) {
+    return AxiosClient.get(`api/group-categories/${categoryID}`).then((response) => response.data);
+  },
+
+  add(data) {
+    return AxiosClient.post(`api/group-categories`, data);
+  },
+
+  update(categoryID, data) {
+    return AxiosClient.patch(`api/group-categories/${categoryID}`, data);
+  },
+
+  delete(categoryID, req) {
+    return AxiosClient.delete(`api/group-categories/${categoryID}`, {
+      data: req
+    });
+  },
+
+  deleteMultiple(req) {
+    return AxiosClient.delete(`api/group-categories`, {
+      data: req
+    });
+  },
+
+  search(req) {
+    return AxiosClient.get(`api/group-categories/find?${makeRequestSearch(req)}`);
+  },
+
+  exportGroupCategory(req) {
+    return AxiosClient.get(`api/group-categories/export?${makeRequestSearch(req)}`, {
+      responseType: "blob"
+    });
+  },
+
+  getPermissions() {
+    return AxiosClient.get(`api/group-categories/permissions`);
+  },
+
+  exportGroupCategorySearch(req) {
+    return AxiosClient.get(`api/group-categories/export/search?${makeRequestSearch(req)}`, {
+      responseType: "blob"
+    });
   }
+};
 
-  static async findById(categoryID) {
-    try {
-      const response = await AxiosClient.get(`api/group-categories/${categoryID}`);
-      return response.data;
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  }
-
-  static async add(data) {
-    try {
-      return await AxiosClient.post(`api/group-categories`, data);
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  }
-
-  static async update(categoryID, data) {
-    try {
-      return await AxiosClient.patch(`api/group-categories/${categoryID}`, data);
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  }
-
-  static async delete(categoryID, req) {
-    try {
-      return await AxiosClient.delete(`api/group-categories/${categoryID}`, {
-        data: req
-      });
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  }
-
-  static async deleteMultiple(req) {
-    try {
-      return await AxiosClient.delete(`api/group-categories`, {
-        data: req
-      });
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  }
-
-  static async search(req) {
-    const reqUri = makeRequestSearch(req);
-
-    const url = `api/group-categories/find?${reqUri}`;
-    try {
-      return await AxiosClient.get(url);
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  }
-
-  static async exportGroupCategory(req) {
-    const reqUri = makeRequestSearch(req);
-    try {
-      return await AxiosClient.get(`api/group-categories/export?${reqUri}`, {
-        responseType: "blob"
-      });
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  }
-
-  static async getPermissions() {
-    try {
-      const response = await AxiosClient.get(`api/group-categories/permissions`);
-      return response;
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  }
-
-  static async exportGroupCategorySearch(req) {
-    const reqUri = makeRequestSearch(req);
-    try {
-      return await AxiosClient.get(`api/group-categories/export/search?${reqUri}`, {
-        responseType: "blob"
-      });
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  }
-}
+export default ErrorWrapper(GroupCategoryApi);
