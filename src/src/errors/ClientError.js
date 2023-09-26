0fd7d3893fb223ad5ed40a5730791b0bdd95c3af
DESKTOP-HKGEPO2\duong <dqvinh20@clc.fitus.edu.vn>
2023-09-26 22:29:36 +0700

    Add custom errors MU-17

---
+++ src/src/errors/ClientError.js
@@ -0,0 +1,10 @@
+import getMessage from "utils/message";
+
+class ClientError extends Error {
+  constructor(clientErrorCode, data) {
+    super(getMessage(parseInt(clientErrorCode, 10), data));
+    this.name = "ClientError";
+  }
+}
+
+export default ClientError;
