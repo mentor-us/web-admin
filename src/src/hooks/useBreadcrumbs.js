f8f9673874e0f9ff11a4f7618662b284117d204c
Dqvinh20 <dqvinh20@clc.fitus.edu.vn>
2023-09-29 22:50:56 +0700

    Add useBreadcrumbs hook MU-17

---
+++ src/src/hooks/useBreadcrumbs.js
@@ -0,0 +1,20 @@
+import { useMemo } from "react";
+import { useLocation } from "react-router-dom";
+
+/**
+ * Hook to get the title and routes path of the current page
+ * @returns {Object} { title, routes } - title: the last element of the path, routes: the path array
+ */
+export default function useBreadcrumbs() {
+  const location = useLocation();
+  return useMemo(() => {
+    const paths = location.pathname.split("/").slice(1);
+    // Remove the last element if it contains "*-detail" (e.g. "group-detail")
+    if (paths.length > 1 && paths.some((path) => path.includes("detail"))) {
+      paths.pop();
+    }
+    const title = paths[paths.length - 1];
+    const routes = paths;
+    return { title, routes };
+  }, [location]);
+}
