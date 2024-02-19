9e06476b94771dc9316e620b0f8c2983399b958a
nguyenhau2506 <nvhau20@clc.fitus.edu.vn>
2024-02-20 00:17:45 +0700

    add userguide mu-242

--- src/src/pages/UserGuide/Mobile/index.jsx
+++ src/src/pages/UserGuide/Mobile/index.jsx
@@ -1,10 +1,17 @@
 import img1 from "assets/images/app-1.png";
 import img2 from "assets/images/app-2.png";
-import img3 from "assets/images/app-3.png";
-import img4 from "assets/images/app-4.png";
-import img5 from "assets/images/app-5.png";
 import img6 from "assets/images/app-6.png";
 import img7 from "assets/images/app-7.png";
+import img8 from "assets/images/app-9.png";
+import img9 from "assets/images/app-10.png";
+import img10 from "assets/images/app-11.png";
+import img11 from "assets/images/app-12.png";
+import img12 from "assets/images/app-13.png";
+import img13 from "assets/images/app-14.png";
+import img14 from "assets/images/app-15.png";
+import img15 from "assets/images/app-16.png";
+import img16 from "assets/images/app-17.png";
+import img17 from "assets/images/app-18.png";
 
 import FullPageLayout from "layouts/FullPageLayout";
 import MDBox from "components/MDComponents/MDBox";
@@ -34,34 +41,120 @@ const info = [
     image: img2
   },
   {
-    title: "Trang chủ ứng dụng",
-    description:
-      "Hiển thị danh sách các nhóm mà tài khoản tham gia làm quản lý (mentor) và làm thành viên (mentee). Bên cạnh đó, màn hình hỗ trợ tìm kiếm nhóm, hiển thị danh sách thông báo về lịch hẹn và công việc của các nhóm mình tham gia.",
-    subDescription:
-      "Dưới mục sự kiện sắp tới sẽ hiển thị danh sách các sự kiện về lịch hẹn, công việc gần thời điểm diễn ra hoặc kết thúc (hiển thị tối đa 5 sự kiện).",
-    isReverse: false,
-    imgWidth: false,
-    image: img3
-  },
-  {
-    title: "Nhắn tin, trao đổi, lịch hẹn, công việc,...",
-    description:
-      "Màn hình hiển thị các tin nhắn của nhóm. Bên cạnh đó cung cấp các chức năng cơ bản khác như gửi ảnh, lên lịch hẹn, tạo bình chọn, tạo định dạng cho tin nhắn, hỗ trợ xóa, sửa, thả biểu cảm,...",
+    category: "Quản lý thông tin cá nhân",
+    title: "Thông tin cá nhân",
+    description: "Hiển thị các thông tin cơ bản của cá nhân cùng chức năng cập nhật và upload ảnh.",
     subDescription: "",
-    isReverse: true,
-    imgWidth: false,
-    image: img4
+    isReverse: false,
+    imgWidth: "400px",
+    image: img7
   },
   {
-    title: "Chi tiết nhóm",
+    category: "Quản lý thông tin cá nhân",
+    title: "Cập nhật thông tin",
     description:
-      "Hiển thị các thông tin liên quan đến nhóm bao gồm tất cả hình ảnh, tệp tin, tin nhắn đã ghim, các bình chọn,... cùng danh sách thành viên, các công việc, lịch hẹn đã và đang xảy ra.",
+      "Để cập nhật thông tin cá nhân, bạn chỉ cần nhấn nút 'Cập nhật' trên màn hình. Tiếp theo, chỉnh sửa thông tin cần thiết và nhấn vào biểu tượng dấu tick để xác nhận và cập nhật thông tin. Quy trình này giúp bạn cập nhật thông tin 1 cách nhanh chóng..",
+    subDescription: "",
+    isReverse: false,
+    imgWidth: "400px",
+    image: img7
+  },
+  {
+    category: "Quản lý thông tin cá nhân",
+    title: "Liên kết email",
+    description:
+      "Để thêm email, bạn chỉ cần nhấn nút 'Thêm email' trên màn hình. Tiếp theo thêm thông tin email cần thêm vào và nhấn vào biểu tượng dấu tick để xác nhận và cập nhật thông tin. Quy trình này giúp bạn thêm thông tin email.",
+    subDescription: "",
+    isReverse: false,
+    imgWidth: "400px",
+    image: img8
+  },
+  {
+    category: "Trang chủ",
+    title: "Phân loại nhóm",
+    description:
+      "Trong ứng dụng MentorUs, nhóm được phân thành hai loại chính: Nhóm Quản lý và Nhóm Thành viên. Trong Nhóm Quản lý, người dùng đảm nhận vai trò của Mentor, không chỉ có các chức năng cơ bản mà còn được trang bị một số quyền đặc biệt. Mentor có khả năng tạo kênh chat riêng, tạo FAQ (Câu hỏi Thường gặp), và ghim thành viên để theo dõi hoạt động của họ một cách hiệu quả.",
     subDescription:
-      "Ngoài ra, FAQ là nơi để các thành viên có thể tạo câu hỏi và câu trả lời chung, có thể tạo mới, cập nhật hoặc import FAQ từ nơi khác.",
+      "Để tham gia vào một nhóm cụ thể, người dùng chỉ cần nhấn vào biểu tượng ở góc trái trên cùng bên phải của màn hình. Từ đó, họ có thể lựa chọn giữa Nhóm Quản lý và Nhóm Thành viên, tùy thuộc vào sự quan tâm và mục tiêu của họ trong cộng đồng MentorUs. Chức năng này giúp người dùng dễ dàng tương tác và tham gia vào nhóm phù hợp với mong muốn và nhu cầu cá nhân của họ.",
     isReverse: false,
     imgWidth: false,
-    image: img5
+    image: img9
   },
+  {
+    category: "Quản lý nhóm",
+    title: "Kênh",
+    description:
+      "Khi bạn nhấn vào một nhóm cụ thể, bạn sẽ được chuyển đến phần quản lý nhóm. Tại đây, bạn có thể lựa chọn kênh mà bạn muốn tham gia để thực hiện trò chuyện.",
+    subDescription:
+      "Kênh cũng cung cấp một không gian hiển thị những tin nhắn riêng tư giữa bạn và một đối tượng cụ thể..",
+    isReverse: false,
+    imgWidth: false,
+    image: img10
+  },
+  {
+    category: "Quản lý nhóm",
+    title: "Bộ sưu tập ảnh và thông tin đã gửi",
+    description:
+      "Trong quản lý nhóm, bạn có thể xem lại tất cả ảnh và tập tin đã được gửi, tạo ra sự thuận tiện và quản lý linh hoạt các nội dung chia sẻ trong nhóm của bạn.",
+    isReverse: false,
+    imgWidth: false,
+    image: img11
+  },
+  {
+    category: "Quản lý nhóm",
+    title: "Thành viên nhóm",
+    description:
+      "Phần 'Thành viên nhóm' giúp bạn dễ dàng xem và quản lý thành viên, bao gồm cả xem thông tin và nhắn tin riêng với từng thành viên trong nhóm.",
+    isReverse: false,
+    imgWidth: false,
+    image: img12
+  },
+  {
+    category: "Quản lý nhóm",
+    title: "Lich hẹn",
+    description:
+      "'Lịch hẹn' giúp bạn xem danh sách và chi tiết các lịch hẹn chưa tới cũng như đã qua, đồng thời cho phép bạn tạo mới các lịch hẹn cho nhóm. Điều này giúp quản lý thời gian và tương tác nhóm một cách hiệu quả.    ",
+    isReverse: false,
+    imgWidth: false,
+    image: img13
+  },
+  {
+    category: "Quản lý nhóm",
+    title: "Công việc",
+    description:
+      "Phần 'Công việc' cho phép bạn xem nhanh các công việc đã giao, của bạn, cũng như công việc sắp tới và đã qua. Bạn cũng có thể tạo công việc mới một cách thuận lợi từ phần này.",
+    isReverse: false,
+    imgWidth: false,
+    image: img14
+  },
+  {
+    category: "Quản lý nhóm",
+    title: "Bảng tin",
+    description:
+      "Để thực hiện bình chọn, bạn có thể sử dụng chức năng bình chọn có sẵn trong ứng dụng. Đơn giản chỉ cần chọn tùy chọn hoặc ý kiến mà bạn muốn bình chọn và xác nhận.",
+    isReverse: false,
+    imgWidth: false,
+    image: img15
+  },
+  {
+    category: "Quản lý nhóm",
+    title: "FAQ",
+    description:
+      "Phần 'FAQ' (Câu hỏi Thường gặp) cung cấp thông tin chi tiết về các câu hỏi mà người dùng thường xuyên đặt. Điều này giúp người dùng tự tìm hiểu và giải quyết vấn đề một cách nhanh chóng mà không cần phải liên hệ trực tiếp. Nếu có thêm câu hỏi, bạn cũng có thể tạo mới để nhận được hỗ trợ chi tiết từ cộng đồng hoặc quản trị viên. Thêm vào đó, bạn còn có khả năng chọn câu hỏi từ nhóm khác, mở rộng khối lượng thông tin hữu ích và đa dạng cho người dùng.",
+    isReverse: false,
+    imgWidth: false,
+    image: img16
+  },
+  {
+    category: "Nhắn tin",
+    description:
+      "Phần 'Nhắn tin' cho phép bạn gửi và nhận tin nhắn, tương tác trực tiếp với thành viên trong nhóm hoặc mentor của mình. Bạn cũng có thể chọn kênh nhắn tin cụ thể và bắt đầu cuộc trò chuyện một cách dễ dàng. ",
+    subDescription: `Những tùy chọn để quản lý tin nhắn (Chỉnh sửa, xóa, Pin, Sao chép, Phản hồi, Chuyển tiếp, React)`,
+    isReverse: false,
+    imgWidth: "400px",
+    image: img17
+  },
+
   {
     title: "Màn hình lịch",
     description:
@@ -71,14 +164,6 @@ const info = [
     isReverse: true,
     imgWidth: false,
     image: img6
-  },
-  {
-    title: "Thông tin cá nhân",
-    description: "Hiển thị các thông tin cơ bản của cá nhân cùng chức năng cập nhật và upload ảnh.",
-    subDescription: "",
-    isReverse: false,
-    imgWidth: "400px",
-    image: img7
   }
 ];
 
@@ -93,6 +178,7 @@ function MobileUserGuide() {
             title={item.title}
             description={item.description}
             subDescription={item.subDescription}
+            category={item.category}
             img={item.image}
             isReverse={item.isReverse}
             imgWidth={item.imgWidth}
--- src/src/pages/UserGuide/components/Instruction/index.jsx
+++ src/src/pages/UserGuide/components/Instruction/index.jsx
@@ -3,7 +3,7 @@ import { PropTypes } from "prop-types";
 
 import "../../styles.css";
 
-function Instruction({ isReverse, title, description, subDescription, img, imgWidth }) {
+function Instruction({ isReverse, title, category, description, subDescription, img, imgWidth }) {
   return (
     <section className="section instruction" id="instruction">
       <div
@@ -20,7 +20,8 @@ function Instruction({ isReverse, title, description, subDescription, img, imgWi
         className="instruction__text animate__animated animate__fadeInDown animate__slow animate__delay-2s"
         style={isReverse ? { order: 1 } : { order: 2 }}
       >
-        <h2>{title}</h2>
+        <h2>{category}</h2>
+        <h3>{title}</h3>
         <p>{description}</p>
         <p>{subDescription}</p>
       </div>
@@ -37,6 +38,8 @@ Instruction.defaultProps = {
 Instruction.propTypes = {
   isReverse: PropTypes.bool,
   title: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+
   description: PropTypes.string.isRequired,
   subDescription: PropTypes.string,
   img: PropTypes.string.isRequired,
