1e5bca1440846fdabbc5ff033afaa917097aa41d
nguyenhau2506 <nvhau20@clc.fitus.edu.vn>
2024-03-15 00:19:19 +0700

    add group info

--- src/src/pages/WebUser/Group/GroupInfo/index.jsx
+++ src/src/pages/WebUser/Group/GroupInfo/index.jsx
@@ -1,19 +1,18 @@
-import React from "react";
-import DraftsIcon from "@mui/icons-material/Drafts";
+import React, { useState } from "react";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import InboxIcon from "@mui/icons-material/Inbox";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import { AccordionDetails } from "@mui/material";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import IconButton from "@mui/material/IconButton";
+import { IconButton } from "@mui/material";
+import Collapse from "@mui/material/Collapse";
+import Divider from "@mui/material/Divider";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+
+import { CalendarIcon, GroupIcon, Hashtag, MediaIcon } from "assets/svgs";
 
 import MDAvatar from "components/MDComponents/MDAvatar";
 // Define the group information
@@ -22,8 +21,71 @@ const groupInfoExample = {
   avatar: "https://sm.ign.com/t/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.300.jpg",
   tools: "haha"
 };
+// eslint-disable-next-line react/prop-types, no-shadow
+function ChannelItem({ type }) {
+  // eslint-disable-next-line react/prop-types
+  switch (type) {
+    case "member":
+      return (
+        <ListItemButton>
+          <ListItemIcon>
+            <GroupIcon width={20} height={20} />
+          </ListItemIcon>
+          <ListItemText
+            disableTypography
+            className="text-base line-clamp-1"
+            primary="Thành viên nhóm"
+          />
+        </ListItemButton>
+      );
+    case "utility":
+      return (
+        <>
+          <ListItemButton>
+            <ListItemIcon>
+              <CalendarIcon width={20} height={20} />
+            </ListItemIcon>
+            <ListItemText disableTypography className="text-base line-clamp-1" primary="Lịch hẹn" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <Hashtag width={20} height={20} />
+            </ListItemIcon>
+            <ListItemText
+              disableTypography
+              className="text-base line-clamp-1"
+              primary="Bình chọn"
+            />
+          </ListItemButton>
+        </>
+      );
+    case "media":
+      return (
+        <>
+          <ListItemButton>
+            <ListItemIcon>
+              <MediaIcon width={20} height={20} />
+            </ListItemIcon>
+            <ListItemText disableTypography className="text-base line-clamp-1" primary="Hình ảnh" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <MediaIcon width={20} height={20} />
+            </ListItemIcon>
+            <ListItemText disableTypography className="text-base line-clamp-1" primary="Hình ảnh" />
+          </ListItemButton>
+        </>
+      );
+    default:
+      return null;
+  }
+}
 
 export default function GroupInfo() {
+  const [ShowGroupMember, setShowGroupMember] = useState(true);
+  const toggleChannelList = () => {
+    setShowGroupMember((pre) => !pre);
+  };
   return (
     <div className="flex flex-col w-80 h-full bg-white border-r-[2px]">
       <div className="w-full font-bold text-base border-white flex justify-center items-center space-x-4 p-4">
@@ -68,68 +130,68 @@ export default function GroupInfo() {
             </div>
           </div>
         </div>
-        <hr className="bg-gray-200 pt-1" />
-        <Accordion defaultExpanded square>
-          <AccordionSummary
-            className="font-bold text-base"
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3-content"
-            id="panel3-header"
-          >
-            Thành viên nhóm
-          </AccordionSummary>
-          <AccordionDetails>
-            <List>
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <InboxIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Inbox" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <DraftsIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Drafts" />
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </AccordionDetails>
-        </Accordion>
-        {/* <hr className="bg-gray-200 pt-1" /> */}
-        <Accordion defaultExpanded square>
-          <AccordionSummary
-            className="font-bold text-base"
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3-content"
-            id="panel3-header"
-          >
-            Bảng tin nhóm
-          </AccordionSummary>
-          <AccordionDetails>
-            <List>
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <InboxIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Inbox" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <DraftsIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Drafts" />
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </AccordionDetails>
-        </Accordion>
+        <Divider
+          sx={{
+            margin: 1,
+            padding: 0,
+            backgroundColor: "#ccc"
+          }}
+        />
+        <ListItemButton onClick={toggleChannelList}>
+          <ListItemText
+            disableTypography
+            className="text-base font-bold"
+            primary="Thành viên nhóm"
+          />
+
+          {ShowGroupMember ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+
+        <Collapse in={ShowGroupMember} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ChannelItem type="member" />
+          </List>
+        </Collapse>
+        <Divider
+          sx={{
+            margin: 1,
+            padding: 0,
+            backgroundColor: "#ccc"
+          }}
+        />
+        <ListItemButton onClick={toggleChannelList}>
+          <ListItemText disableTypography className="text-base font-bold" primary="Bảng tin" />
+
+          {ShowGroupMember ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+
+        <Collapse in={ShowGroupMember} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ChannelItem type="utility" />
+          </List>
+        </Collapse>
+        <Divider
+          sx={{
+            margin: 1,
+            padding: 0,
+            backgroundColor: "#ccc"
+          }}
+        />
+        <ListItemButton onClick={toggleChannelList}>
+          <ListItemText
+            disableTypography
+            className="text-base font-bold"
+            primary="Bộ sưu tập, tập tin đã gửi"
+          />
+
+          {ShowGroupMember ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+
+        <Collapse in={ShowGroupMember} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ChannelItem type="media" />
+          </List>
+        </Collapse>
       </div>
     </div>
   );
