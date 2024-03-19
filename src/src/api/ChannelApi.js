de8bc7a76ed0ba4fff7b0fcda9c76707bfd939e6
nguyenhau2506 <nvhau20@clc.fitus.edu.vn>
2024-03-20 00:33:15 +0700

    doing media list

--- src/src/api/ChannelApi.js
+++ src/src/api/ChannelApi.js
@@ -5,6 +5,11 @@ const ChannelApi = {
     const URL = `/api/channels/${memberId}/members`;
     const data = await AxiosClient.get(URL);
     return data;
+  },
+  async getChannelMedia(memberId) {
+    const URL = `/api/groups/${memberId}/media`;
+    const data = await AxiosClient.get(URL);
+    return data?.data;
   }
 };
 export default ChannelApi;
--- src/src/hooks/channels/queries.js
+++ src/src/hooks/channels/queries.js
@@ -3,13 +3,18 @@ import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 import channelService from "service/channelService";
 
-import { GetChannelMembersKey } from "./keys";
+import { GetChannelMediaKey, GetChannelMembersKey } from "./keys";
 
 // eslint-disable-next-line import/prefer-default-export
 export const useGetGroupMembers = (groupId) =>
-  console.log("groupId", groupId) ||
   useQuery({
     queryKey: GetChannelMembersKey(groupId),
     queryFn: () => channelService.getChannelMembers(groupId),
+    enabled: !!groupId
+  });
+export const useGetGroupMedia = (groupId) =>
+  useQuery({
+    queryKey: GetChannelMediaKey(groupId),
+    queryFn: () => channelService.getChannelMedia(groupId),
     enabled: !!groupId
   });
