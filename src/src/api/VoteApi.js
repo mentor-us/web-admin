e508194d75eb1befd3e8edd0d46c36a59d3a9448
nguyenhau2506 <nvhau20@clc.fitus.edu.vn>
2024-04-04 22:23:28 +0700

    fix path

--- src/src/api/VoteApi.js
+++ src/src/api/VoteApi.js
@@ -5,7 +5,7 @@ const VOTE_URL = "/api/votes";
 const VoteApi = {
   createVote: (vote) => AxiosClient.post(VOTE_URL, vote), // Added comma at the end
   async getAllVotesByGroupId(channelId) {
-    const URL = `/api/votes?groupId=${channelId}`;
+    const URL = `/api/channels/${channelId}/votes`;
     const data = await AxiosClient.get(URL); // Destructure data from the response
     return data;
   }
