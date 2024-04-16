c34df9b7e7002190bbacc83be4eeb3ed564c3329
nguyenhau2506 <nvhau20@clc.fitus.edu.vn>
2024-04-16 20:51:05 +0700

    change component task

--- src/src/pages/WebUser/Group/GroupInfo/GroupItemDetail/GroupTask/TaskItem/index.jsx
+++ src/src/pages/WebUser/Group/GroupInfo/GroupItemDetail/GroupTask/TaskItem/index.jsx
@@ -11,24 +11,21 @@ function TaskItem({ task }) {
   const [openDialogTask, setOpenDialogTask] = useState(false);
 
   return (
-    <Box className="!px-2 hover:scale-105">
+    <Box className="!px-2 ">
       <Box
         onClick={() => {
           setOpenDialogTask(true);
         }}
-        className="w-95 max-w-screen-md shadow-md my-1 gap-1  p-3 pt-10 relative hover:cursor-pointer"
+        className="w-full max-w-screen-md hover:!shadow-lg !border-solid border-2 my-1 gap-1 rounded-xl p-3 pt-10 relative hover:cursor-pointer"
       >
-        <Box className="bg-orange-500 absolute top-1 right-0 px-2 py-2 ">
-          <p className="text-white  text-xs text-center">
-            {formateTask?.deadlineTimeModel?.displayName}
-          </p>
+        <Box className="bg-orange-500  absolute top-0 right-0 px-2 py-2 rounded-tr-xl rounded-bl-xl">
+          <p className="text-white text-sm font-bold">Công việc</p>
         </Box>
-        <Box className="absolute top-1 left-0 px-2 py-2 border-l-4 border-orange-500 rounded-r-md">
-          <p className="text-orange-500 text-sm font-bold">Công việc</p>
+        <Box className="absolute top-1 left-0 px-2 py-2  rounded-r-md">
+          <p className="text-black text-xl">{formateTask?.title}</p>
         </Box>
-        <p className="text-black text-xl">{formateTask?.title}</p>
         <Box className="flex flex-row justify-between">
-          <p className="text-gray-500 text-sm">Nhóm: {formateTask?.channel?.name} </p>
+          <p className="text-gray-500 text-sm">{formateTask?.deadlineTimeModel?.displayName}</p>
           <p className="text-gray-500 text-sm">
             {TaskStatusObject[formateTask.status]?.displayName}
           </p>
