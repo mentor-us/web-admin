e8d46cdfbe6555a759d869ba27d139a8cf8fba81
nguyenhau2506 <nvhau20@clc.fitus.edu.vn>
2024-04-22 22:53:37 +0700

    add key FAQ MU-293

--- src/src/hooks/chats/keys.js
+++ src/src/hooks/chats/keys.js
@@ -5,5 +5,6 @@ export const AddReactionMutationKey = "add-reaction";
 export const RemoveReactionMutationKey = "remove-reaction";
 export const EditMessageMutationKey = "edit-message";
 export const DeleteMessageMutationKey = "delete-message";
+export const CreateFaqMutationKey = "create-faq";
 
 export const GetAllChatMessageInfinityKey = (channelId) => ["chat-message-infinity-key", channelId];
