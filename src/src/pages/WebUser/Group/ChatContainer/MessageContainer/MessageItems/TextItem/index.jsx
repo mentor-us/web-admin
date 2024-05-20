6ae563b557f3fca3f914d74668ed04ba12ddb926
thong89x <thong89x@gmail.com>
2024-05-20 22:46:23 +0700

    MU-332 add forward sender name

--- src/src/pages/WebUser/Group/ChatContainer/MessageContainer/MessageItems/TextItem/index.jsx
+++ src/src/pages/WebUser/Group/ChatContainer/MessageContainer/MessageItems/TextItem/index.jsx
@@ -8,45 +8,52 @@ import MessageItemContainer from "../../MessageItemContainer";
 
 function TextItem({ message, isOwner }) {
   return (
-    <MessageItemContainer isOwner={isOwner} message={message}>
-      {message?.reply && (
-        <Box className="border-l-2 px-1 border-[#006EDC]">
-          <Typography className="!text-xs font-bold text-[#006EDC]">
-            {message.reply?.senderName}
-          </Typography>
-          <ListItemText
-            primary={
-              <div
-                className="text-xs text-nowrap !text-[#444] !line-clamp-5"
-                style={{
-                  display: "block",
-                  overflow: "hidden",
-                  whiteSpace: "initial",
-                  textOverflow: "ellipsis",
-                  wordBreak: "break-word"
-                }}
-                dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(message.reply?.content) }}
-              />
-            }
-          />
-        </Box>
+    <div className="flex flex-col">
+      {message.isForward && (
+        <span className={` text-xs ${isOwner ? "text-right  pr-4" : "text-left pl-12"}`}>
+          {isOwner ? "Bạn" : message.sender.name} đã chuyển tiếp một tin nhắn
+        </span>
       )}
-      <ListItemText
-        primary={
-          <div
-            className="text-sm text-nowrap"
-            style={{
-              display: "block",
-              overflow: "hidden",
-              whiteSpace: "initial",
-              textOverflow: "ellipsis",
-              wordBreak: "break-word"
-            }}
-            dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(message.content) }}
-          />
-        }
-      />
-    </MessageItemContainer>
+      <MessageItemContainer isOwner={isOwner} message={message}>
+        {message?.reply && (
+          <Box className="border-l-2 px-1 border-[#006EDC]">
+            <Typography className="!text-xs font-bold text-[#006EDC]">
+              {message.reply?.senderName}
+            </Typography>
+            <ListItemText
+              primary={
+                <div
+                  className="text-xs text-nowrap !text-[#444] !line-clamp-5"
+                  style={{
+                    display: "block",
+                    overflow: "hidden",
+                    whiteSpace: "initial",
+                    textOverflow: "ellipsis",
+                    wordBreak: "break-word"
+                  }}
+                  dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(message.reply?.content) }}
+                />
+              }
+            />
+          </Box>
+        )}
+        <ListItemText
+          primary={
+            <div
+              className="text-sm text-nowrap"
+              style={{
+                display: "block",
+                overflow: "hidden",
+                whiteSpace: "initial",
+                textOverflow: "ellipsis",
+                wordBreak: "break-word"
+              }}
+              dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(message.content) }}
+            />
+          }
+        />
+      </MessageItemContainer>
+    </div>
   );
 }
 
