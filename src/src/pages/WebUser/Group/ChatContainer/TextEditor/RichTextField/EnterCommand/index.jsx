e96b563f920d2dfe82c7285f611b1357c3e954e8
Duong Quang Vinh <dqvinh20@clc.fitus.edu.vn>
2024-05-23 22:47:42 +0700

    MU-346 Fix user can send empty string on press Enter

--- src/src/pages/WebUser/Group/ChatContainer/TextEditor/RichTextField/EnterCommand/index.jsx
+++ src/src/pages/WebUser/Group/ChatContainer/TextEditor/RichTextField/EnterCommand/index.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable no-unused-vars */
 import { useEffect } from "react";
 import { $generateHtmlFromNodes } from "@lexical/html";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { useLexicalIsTextContentEmpty } from "@lexical/react/useLexicalIsTextContentEmpty";
 
 import { CLEAR_EDITOR_COMMAND, COMMAND_PRIORITY_HIGH, KEY_ENTER_COMMAND } from "lexical";
 
 function EnterCommand({ onEnter = () => {} }) {
   const [editor] = useLexicalComposerContext();
+  const isEmpty = useLexicalIsTextContentEmpty(editor, true);
 
   useEffect(() => {
     return editor.registerCommand(
@@ -19,6 +22,7 @@ function EnterCommand({ onEnter = () => {} }) {
           const editorState = editor.getEditorState();
 
           editorState.read(() => {
+            if (isEmpty) return;
             const htmlString = $generateHtmlFromNodes(editor);
             onEnter(htmlString);
           });
@@ -30,7 +34,7 @@ function EnterCommand({ onEnter = () => {} }) {
       },
       COMMAND_PRIORITY_HIGH
     );
-  }, [editor, onEnter]);
+  }, [editor, onEnter, isEmpty]);
 
   return null;
 }
