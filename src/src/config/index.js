d89f8370b94a52815785dbba2daab70b2a0769fa
nguyenhau2506 <nvhau20@clc.fitus.edu.vn>
2024-06-13 22:44:54 +0700

    add socket url to env

--- src/src/config/index.js
+++ src/src/config/index.js
@@ -4,3 +4,4 @@ export const SUPPORT_EMAIL = import.meta.env.REACT_APP_SUPPORT_EMAIL;
 export const REGISTER_FORM_URL = import.meta.env.REACT_APP_REGISTER_FORM_URL;
 export const SECRECT_API_KEY_CALENDAR = import.meta.env.REACT_APP_SECRECT_API_KEY_CALENDAR;
 export const CALENDAR_ID = import.meta.env.REACT_APP_CALENDAR_ID;
+export const SOCKET_URL = import.meta.env.REACT_APP_SOCKET_URL;
