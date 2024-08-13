d0343cf6241f0d21a6a2180e953ea8cc8beb4832
thong89x <thong89x@gmail.com>
2024-08-13 23:26:51 +0700

    fix bug calendar

--- src/src/hooks/events/queries.js
+++ src/src/hooks/events/queries.js
@@ -17,7 +17,8 @@ export const useGetAllEvents = () =>
           return {
             ...event,
             start: new Date(event.deadline ?? event.timeStart),
-            timeStart: event.deadline ?? event.timeStart
+            timeStart: event.deadline ?? event.timeStart,
+            end: new Date(event.upcomingTime).setHours(23, 59, 0, 0)
           };
         });
         return events;
